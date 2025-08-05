55c1498413576bc5894532d950265c50e4a3710e
smsohelrana420 <mirzasohel550@gmail.com>
2025-08-05 22:45:42 +0600

    login page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 
 import React from 'react'
-
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Not_Found from './pages/Not_Found';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path:"*",
+    element:<Not_Found/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  }
+  ,
+  {
+    path:"/signup",
+    element:<SignUp/>
+  }
+]);
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
